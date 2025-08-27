3987553ee2f5ba9929d8fd61b1d2783b2991221e
Thesirix <123495820+Thesirix@users.noreply.github.com>
2025-08-27 20:09:01 +0200

    feat: config pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: "app",
-      script: "./www/app.js",
-      env_production: {
+      name: "node-approfondissement",
+      script: "www/app.js",
+      exec_mode: "cluster",
+      instances: 3,
+      max_memory_restart: "200M",
+
+      // Logs
+      error_file: "./logs/err.log",
+      out_file: "./logs/out.log",
+
+      env: {
         NODE_ENV: "production",
+        PORT: 3000,
       },
     },
   ],
